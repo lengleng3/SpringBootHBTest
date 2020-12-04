dc1d6899d65132dffaf49de16cdf20476a768d6b
HuBing <1786932409@qq.com>
2020-12-04 09:40:13 +0800

    first commit

---
+++ src/main/java/com/icelake/springboot/test/application/StartSpringBootHBTest.java
@@ -0,0 +1,8 @@
+package com.icelake.springboot.test.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.icelake.springboot"})
+
+public class StartSpringBootHBTest {
+}
